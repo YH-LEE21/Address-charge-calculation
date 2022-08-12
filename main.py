06933d352adbaebb0c1511556728fa5c1fc06a43
YH-LEE21 <ksmwy9029@naver.com>
2022-08-12 15:43:13 +0900

    encoding注意

--- main.py
+++ main.py
@@ -16,31 +16,43 @@ with open('SampleData.csv', encoding='utf-8', newline='') as csvfile:
 
         # adressが東京都の場合
         if address.find('東京都') != -1:
-            print('{会社名:' + row['会社名'] + '\t住所:' + row['住所'] + '\t料金:'+str(95) + '円}')
+            print('会社名:' + row['会社名'])
+            print('住所:' + row['住所'])
+            print('料金:' + str(95) + '円')
             count1 += 1
             price1 += 95
 
         # adressが千葉の場合
         elif address.find('千葉県') != -1:
-            print('{会社名:' + row['会社名'] + '\t住所:' + row['住所'] + '\t料金:'+str(105) + '円}')
+            print('会社名:' + row['会社名'])
+            print('住所:' + row['住所'])
+            print('料金:' + str(95) + '円')
+
             count2[0] += 1
             price2[0] += 105
 
         # adressが埼玉の場合
         elif address.find('埼玉県') != -1:
-            print('{会社名:' + row['会社名'] + '\t住所:' + row['住所'] + '\t料金:'+str(105) + '円}')
+            print('会社名:' + row['会社名'])
+            print('住所:' + row['住所'])
+            print('料金:' + str(95) + '円')
             count2[1] += 1
             price2[1] += 105
         # adressが神奈川の場合
         elif address.find('神奈川県') != -1:
-            print('{会社名:' + row['会社名'] + '\t住所:' + row['住所'] + '\t料金:'+str(105) + '円}')
+            print('会社名:' + row['会社名'])
+            print('住所:' + row['住所'])
+            print('料金:' + str(95) + '円')
             count2[2] += 1
             price2[2] += 105
         # その他の場合
         else:
-            print('{会社名:' + row['会社名'] + '\t住所:' + row['住所'] + '\t料金:'+str(115) + '円}')
+            print('会社名:' + row['会社名'])
+            print('住所:' + row['住所'])
+            print('料金:' + str(95) + '円')
             count3 += 1
             price3 += 115
+        print()
 
 print()
 print("東京都件 :"+str(count1)+"件\t総価格:"+str(price1)+"円")
